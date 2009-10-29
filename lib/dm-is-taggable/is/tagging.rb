a96142d3aaabf9441cd73f087330a88e67e17f96
snusnu <gamsnjaga@gmail.com>
2009-10-29 05:09:26 +0100

    FK property types match the referenced column types

--- lib/dm-is-taggable/is/tagging.rb
+++ lib/dm-is-taggable/is/tagging.rb
@@ -2,7 +2,7 @@ module Tagging
   include DataMapper::Resource
   
   property :id, Serial
-  property :tag_id, Integer, :nullable => false
+  property :tag_id, Integer, :min => 0, :nullable => false
   
   is :remixable, :suffix => "tag"
 end
