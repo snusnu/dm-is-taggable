1923a0653f7226cba11386d1b72aae20a27b369b
snusnu <gamsnjaga@gmail.com>
2010-03-16 03:34:30 +0100

    Makes tag names required and have a unique index

--- lib/dm-is-taggable/is/tag.rb
+++ lib/dm-is-taggable/is/tag.rb
@@ -1,8 +1,8 @@
 class Tag
   include DataMapper::Resource
 
-  property :id, Serial
-  property :name, String, :unique => true
+  property :id,   Serial
+  property :name, String, :required => true, :unique => true, :unique_index => true
 
   before :save, :strip_name
 
